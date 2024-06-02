6c8cdda0486adb2e864aa5085d48badd0de637da
TUANDAT\dangt <dangtuandat99@gmail.com>
2024-06-02 23:20:44 +0700

    Add auth interface

---
+++ components/FormField.tsx
@@ -0,0 +1,35 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { icons } from '@/constants';
+
+const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, ...pros }: any) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <View className={`space-y-2 ${otherStyles}`}>
+            <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
+            <View
+                className='w-full h-16 border-2 border-black-200 bg-black-100 rounded-2xl focus:border-secondary flex-row'
+            >
+                <TextInput className='flex-1 px-4 text-white font-semibold text-base'
+                    value={value}
+                    placeholder={placeholder}
+                    placeholderTextColor="#7b7b8b"
+                    onChangeText={handleChangeText}
+                    secureTextEntry={title === 'Password' && !showPassword}
+                />
+                <View
+                    className='flex-row items-center pr-4'
+                >
+                    {title === 'Password' && (
+                        <TouchableOpacity onPress={() => setShowPassword(!showPassword)} >
+                            <Image source={!showPassword ? icons.eye : icons.eyeHide} className='w-6 h-6' resizeMode='contain' />
+                        </TouchableOpacity>
+                    )}
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default FormField
