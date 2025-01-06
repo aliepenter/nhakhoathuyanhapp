12a8e136b69d25fc17ece4b0a2dc8d107ee25841
TUANDAT\dangt <dangtuandat99@gmail.com>
2025-01-06 13:46:47 +0700

    Create before after screen

---
+++ screens/home/image-gallery/beforeAfter.screen.tsx
@@ -0,0 +1,11 @@
+import { View, Text, ScrollView, RefreshControl, Pressable, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { formatDate } from '@/lib/commonFunctions';
+import { router } from 'expo-router';
+import { getAnhQuaTrinh } from '@/lib/apiCall';
+
+export default function BeforeAfterScreen({ user }: any) {
+  return (
+    <View></View>
+  )
+}
